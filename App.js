c5941a2dca5bdccb8711a52159d2d5092f54a10a
Olivier Martineau <omartineau8@gmail.com>
2019-07-06 11:51:44 -0400

    Updated App.js

--- App.js
+++ App.js
@@ -1,62 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import { Navigation, createDrawerNavigator } from 'react-navigation';
+import Home from './Screens/Home.js';
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+export default class App extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>We are going to make a drawer</Text>
+            </View>
+        );
+    }
+}
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App = () => {
-  return (
-    <View>
-        <Text>Hello Worlds</Text>
-    </View>
-  );
-};
+//Navigation.registerComponent('drawer.Home', () => Home);
+//
+//Navigation.startSingleScreenApp({
+//  screen: {
+//    screen: 'drawer.Home',
+//    title: 'Home'
+//  }
+//});
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+//const styles = StyleSheet.create({
+//  scrollView: {
+//    backgroundColor: Colors.lighter,
+//  },
+//  body: {
+//    backgroundColor: Colors.white,
+//  },
+//  sectionContainer: {
+//    marginTop: 32,
+//    paddingHorizontal: 24,
+//  },
+//  sectionTitle: {
+//    fontSize: 24,
+//    fontWeight: '600',
+//    color: Colors.black,
+//  },
+//  sectionDescription: {
+//    marginTop: 8,
+//    fontSize: 18,
+//    fontWeight: '400',
+//    color: Colors.dark,
+//  },
+//  highlight: {
+//    fontWeight: '700',
+//  },
+//});
 
-export default App;
+
--- Screens/Home.js
+++ Screens/Home.js
@@ -5,12 +5,17 @@ import {
 } from 'react-native';
 
 class Home extends Component {
+
+onButtonPress = () => {
+    console.log('Button Pressed!!')
+  }
   render () {
     return (
       <View style={{padding: 50}}>
         <Text>
           Home
         </Text>
+        <Button title="Tab Navigation" onPress = { this.onButtonPress } />
       </View>
     );
   }
